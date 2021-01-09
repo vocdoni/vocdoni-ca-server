7a112bd9155f089a18ed0d049a354c9bdb5cca77
p4u <pau@dabax.net>
2021-01-09 19:36:35 +0100

    add certificate time validity check and minor changes
    Signed-off-by: p4u <pau@dabax.net>

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"flag"
 
+	"github.com/google/uuid"
 	"github.com/vocdoni/multirpc/transports/mhttp"
 	"go.vocdoni.io/dvote/crypto/ethereum"
 	"go.vocdoni.io/dvote/db"
@@ -48,45 +49,59 @@ func (ah *authHandler) AuthHandler(w http.ResponseWriter, r *http.Request) {
 	log.Infof(r.UserAgent())
 	orgID := strings.TrimPrefix(r.URL.EscapedPath(), "/auth/")
 	if len(orgID) == 0 {
-		w.Write([]byte("error: no organization provided"))
+		w.Write([]byte("<p>error: no organization provided</p>"))
 		return
 	}
 	if r.TLS != nil && len(r.TLS.PeerCertificates) > 0 {
+		// Get client certificate content
 		content, err := json.MarshalIndent(r.TLS.PeerCertificates[0], "", " ")
 		if err != nil {
-			io.WriteString(w, err.Error())
+			log.Warnf("cannot marshal TLS identity: %v", err)
+			w.Write([]byte(err.Error()))
 			return
 		}
+		// Unmarshal certificate content
 		ic := new(idCat)
 		if err := json.Unmarshal(content, ic); err != nil {
 			log.Debugf("%s", content)
 			log.Errorf("json unmarshal error: %v", err)
+			w.Write([]byte("<p>error: cannot parse json certificate data</p>"))
 			return
 		}
+		// Check certificate time
+		if time.Now().Unix() > ic.NotAfter.Unix() || time.Now().Unix() < ic.NotBefore.Unix() {
+			log.Warnf("certificate issued for wrong date")
+			w.Write([]byte("<p>error: wrong certificate validity date</p>"))
+			return
+		}
+		// Compute unique hash and check if already exist
 		ichash := ic.Hash()
 		var dbkey bytes.Buffer
 		dbkey.Write([]byte(orgID))
 		dbkey.Write(ichash)
-		exist, err := ah.kv.Has(dbkey.Bytes())
-		if err != nil {
-			log.Errorf("cannot feth db: %v", err)
+		userToken, err := ah.kv.Get(dbkey.Bytes())
+		if err != nil && err.Error() != "Key not found" {
+			log.Errorf("cannot fetch db: %v", err)
 			return
 		}
-		if exist {
+		if userToken != nil {
 			log.Infof("user already registered on entity %s (hash id: %x", orgID, ichash)
-			w.Write([]byte(fmt.Sprintf("error: already registered, your token is %x", ichash)))
+			w.Write([]byte(fmt.Sprintf("<p>already registered, your token is %s</p>", userToken)))
 			return
 		}
-
-		if _, err := w.Write([]byte(fmt.Sprintf("Registration successful for %s, your token for organization %s is %x", ic.Subject.CommonName, orgID, ichash))); err != nil {
+		// Register certificate and give it a token
+		newToken := uuid.New()
+		if _, err := w.Write([]byte(
+			fmt.Sprintf("<p><strong>Registration successful</strong> for %s.<br/>Your token for organization %s is %s</p>",
+				ic.Subject.CommonName, orgID, newToken.String()))); err != nil {
 			log.Errorf("error writing: %v", err)
 			return
 		}
-		if err := ah.kv.Put(dbkey.Bytes(), []byte{}); err != nil {
-			log.Errorf("error storing hash: %v", err)
+		if err := ah.kv.Put(dbkey.Bytes(), []byte(newToken.String())); err != nil {
+			log.Errorf("<p>error storing hash: %v</p>", err)
 			return
 		}
-
+		// For console log pretty print
 		content, err = json.Marshal(ic)
 		if err != nil {
 			log.Errorf("json marshal error: %v", err)
@@ -95,7 +110,7 @@ func (ah *authHandler) AuthHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%s\n", content)
 	} else {
 		log.Warnf("no certificate found")
-		w.Write([]byte("error: no certificates found"))
+		w.Write([]byte("<p>error: no certificates found</p>"))
 	}
 }
 
@@ -122,8 +137,8 @@ type idCat struct {
 		SerialNumber       string
 		CommonName         string
 	}
-	NotBefore string
-	NotAfter  string
+	NotBefore time.Time
+	NotAfter  time.Time
 }
 
 func (ic *idCat) Hash() []byte {
